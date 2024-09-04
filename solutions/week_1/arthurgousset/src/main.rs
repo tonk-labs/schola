d757c3186e12f614d0b12ddd0bb6d043c4608cda
Arthur Gousset <46296830+arthurgousset@users.noreply.github.com>
2024-09-04 11:17:55 +0100

    feat: assigns primes `p` and `q`
    I'm using the first two primes after 1,000,000.
    
    E.g.
    
    Prime p: 15485867
    Prime q: 15485917

--- solutions/week_1/arthurgousset/src/main.rs
+++ solutions/week_1/arthurgousset/src/main.rs
@@ -2,8 +2,10 @@ use primes::{PrimeSet, Sieve};
 
 fn main() {
     let mut pset = Sieve::new();
+    let primes = pset.iter().skip(1_000_000).take(2).collect::<Vec<_>>();
+    let p = primes[0];
+    let q = primes[1];
 
-    for (ix, n) in pset.iter().enumerate().take(10) {
-        println!("Prime {}: {}", ix, n);
-    }
+    println!("Prime p: {}", p);
+    println!("Prime q: {}", q);
 }
