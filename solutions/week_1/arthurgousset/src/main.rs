b95027cf38952cc86089cc0f1d3136795b4045d3
Arthur Gousset <46296830+arthurgousset@users.noreply.github.com>
2024-09-04 11:25:49 +0100

    refactor: move code into `generate_prime_numbers()` function

--- solutions/week_1/arthurgousset/src/main.rs
+++ solutions/week_1/arthurgousset/src/main.rs
@@ -1,6 +1,6 @@
 use primes::{PrimeSet, Sieve};
 
-fn main() {
+fn generate_prime_numbers() -> (u64, u64) {
     let mut pset = Sieve::new();
     let primes = pset.iter().skip(1_000_000).take(2).collect::<Vec<_>>();
     let p = primes[0];
@@ -8,4 +8,11 @@ fn main() {
 
     println!("Prime p: {}", p);
     println!("Prime q: {}", q);
+
+    (p, q)
+}
+
+fn main() {
+    let (p, q) = generate_prime_numbers();
+    println!("Successfully deconstructed values {} and {}", p, q);
 }
