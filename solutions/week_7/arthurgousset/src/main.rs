5b6e8ccf4d8c45bfda7bfe0b5e47e32d17a9833b
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-11-07 21:52:13 +0000

    feat: computes polynomial coefficients for all vectors

--- solutions/week_7/arthurgousset/src/main.rs
+++ solutions/week_7/arthurgousset/src/main.rs
@@ -1,5 +1,5 @@
 use num_bigint::BigInt;
-use week_7_arthurgousset::{mod_prime, srs::setup_srs, mod_inverse};
+use week_7_arthurgousset::{mod_inverse, mod_prime, srs::setup_srs};
 
 fn compute_interpolation_matrix(domain: &[BigInt], modulus: &BigInt) -> Vec<Vec<BigInt>> {
     let n = domain.len();
@@ -9,7 +9,7 @@ fn compute_interpolation_matrix(domain: &[BigInt], modulus: &BigInt) -> Vec<Vec<
     // Each row i represents: [1, x_i, x_i^2, x_i^3]
     for i in 0..n {
         let x = &domain[i];
-        matrix[i][0] = BigInt::from(1);  // x^0
+        matrix[i][0] = BigInt::from(1); // x^0
         matrix[i][1] = mod_prime(&x, modulus); // x^1
         matrix[i][2] = mod_prime(&(&matrix[i][1] * x), modulus); // x^2
         matrix[i][3] = mod_prime(&(&matrix[i][2] * x), modulus); // x^3
@@ -22,7 +22,7 @@ fn compute_interpolation_matrix(domain: &[BigInt], modulus: &BigInt) -> Vec<Vec<
 fn invert_matrix(matrix: &Vec<Vec<BigInt>>, modulus: &BigInt) -> Vec<Vec<BigInt>> {
     let n = matrix.len();
     let mut augmented = vec![vec![BigInt::from(0); 2 * n]; n];
-    
+
     // Create augmented matrix [A|I]
     for i in 0..n {
         for j in 0..n {
@@ -38,7 +38,7 @@ fn invert_matrix(matrix: &Vec<Vec<BigInt>>, modulus: &BigInt) -> Vec<Vec<BigInt>
         let pivot_inv = mod_inverse(pivot, modulus);
 
         // Scale row i
-        for j in 0..2*n {
+        for j in 0..2 * n {
             augmented[i][j] = mod_prime(&(&augmented[i][j] * &pivot_inv), modulus);
         }
 
@@ -46,7 +46,7 @@ fn invert_matrix(matrix: &Vec<Vec<BigInt>>, modulus: &BigInt) -> Vec<Vec<BigInt>
         for k in 0..n {
             if k != i {
                 let factor = augmented[k][i].clone();
-                for j in 0..2*n {
+                for j in 0..2 * n {
                     let temp = mod_prime(&(&factor * &augmented[i][j]), modulus);
                     augmented[k][j] = mod_prime(&(&augmented[k][j] - &temp), modulus);
                 }
@@ -63,6 +63,24 @@ fn invert_matrix(matrix: &Vec<Vec<BigInt>>, modulus: &BigInt) -> Vec<Vec<BigInt>
     }
 
     inverse
+}
+
+fn compute_polynomial_coefficients(
+    values: &[BigInt],
+    domain: &[BigInt],
+    modulus: &BigInt,
+) -> Vec<BigInt> {
+    let interpolation_matrix = compute_interpolation_matrix(domain, modulus);
+    let mut coefficients = vec![BigInt::from(0); 4];
+
+    for i in 0..4 {
+        for j in 0..4 {
+            let term = mod_prime(&(&interpolation_matrix[i][j] * &values[j]), modulus);
+            coefficients[i] = mod_prime(&(&coefficients[i] + &term), modulus);
+        }
+    }
+
+    coefficients
 }
 
 fn main() {
@@ -122,7 +140,6 @@ fn main() {
         BigInt::from(25),
         BigInt::from(25),
     ];
-    
 
     // Roots of Unity
     // We happen to know that the 4th roots of unity in F_{17} are 1, 4, 13, 16.
@@ -147,7 +164,8 @@ fn main() {
         .map(|h| mod_prime(&(h * &k2), &modulus))
         .collect::<Vec<BigInt>>();
 
-    println!("\nK1H: {:?}", K1H);
+    println!("\nH: {:?}", H);
+    println!("K1H: {:?}", K1H);
     println!("K2H: {:?}", K2H);
 
     // Interpolating Using the Roots of Unity
@@ -156,22 +174,24 @@ fn main() {
     // We want f_a(1) = 3, f_a(4) = 4, f_a(16) = 5, f_a(13) = 9
     // Compute interpolation matrix
 
-    let interpolation_matrix = compute_interpolation_matrix(&H, &modulus);
-    
-    println!("\nInterpolation Matrix:");
-    for row in &interpolation_matrix {
-        println!("{:?}", row);
-    }
+    // Compute polynomial coefficients for each vector
+    let coeffs_a = compute_polynomial_coefficients(&a, &H, &modulus);
+    let coeffs_b = compute_polynomial_coefficients(&b, &H, &modulus);
+    let coeffs_c = compute_polynomial_coefficients(&c, &H, &modulus);
+    let coeffs_qL = compute_polynomial_coefficients(&qL, &H, &modulus);
+    let coeffs_qR = compute_polynomial_coefficients(&qR, &H, &modulus);
+    let coeffs_qO = compute_polynomial_coefficients(&qO, &H, &modulus);
+    let coeffs_qM = compute_polynomial_coefficients(&qM, &H, &modulus);
+    let coeffs_qC = compute_polynomial_coefficients(&qC, &H, &modulus);
 
-    // Example: interpolate polynomial for vector a
-    let mut coefficients_a = vec![BigInt::from(0); 4];
-    for i in 0..4 {
-        for j in 0..4 {
-            let term = mod_prime(&(&interpolation_matrix[i][j] * &a[j]), &modulus);
-            coefficients_a[i] = mod_prime(&(&coefficients_a[i] + &term), &modulus);
-        }
-    }
-
-    println!("\nCoefficients for polynomial a:");
-    println!("{:?}", coefficients_a);
+    // Print results
+    println!("\nPolynomial Coefficients [d, c, b, a] where f(x) = ax³ + bx² + cx + d:");
+    println!("a: {:?}", coeffs_a);
+    println!("b: {:?}", coeffs_b);
+    println!("c: {:?}", coeffs_c);
+    println!("qL: {:?}", coeffs_qL);
+    println!("qR: {:?}", coeffs_qR);
+    println!("qO: {:?}", coeffs_qO);
+    println!("qM: {:?}", coeffs_qM);
+    println!("qC: {:?}", coeffs_qC);
 }
